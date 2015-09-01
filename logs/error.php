73e940e4a72746bb93f67afff9866d2b2dd966c0
shamusdougan <shamus@douganfamily.com.au>
2015-09-01 15:30:06 +1000

    Update to get EVERYTHING this time :(

---
+++ logs/error.php
@@ -0,0 +1,18 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-06-09 01:06:36 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-06-09T01:06:36+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-06-09T01:06:39+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-09-04T02:01:16+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-09-09T00:25:41+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-10-07T23:14:39+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-10-07T23:14:43+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-10-07T23:14:47+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-10-07T23:14:53+00:00	INFO	joomlafailure	Empty password not allowed
+2014-10-07T23:15:02+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-10-07T23:16:17+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-10-07T23:16:24+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-10-07T23:16:28+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
---
+++ logs/joomla_update.php
@@ -0,0 +1,43 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-06-10 23:27:42 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-06-10T23:27:42+00:00	INFO	update	Update started by user Super User (307). Old version is 3.2.3.
+2014-06-10T23:27:43+00:00	INFO	update	Downloading update file from .
+2014-06-10T23:28:09+00:00	INFO	update	File Joomla_3.3.0-Stable-Update_Package.zip successfully downloaded.
+2014-06-10T23:28:09+00:00	INFO	update	Starting installation of new version.
+2014-06-10T23:28:45+00:00	INFO	update	Finalising installation.
+2014-06-10T23:28:47+00:00	INFO	update	Ran query from file 3.3.0-2014-02-16. Query text: ALTER TABLE `#__users` ADD COLUMN `requireReset` tinyint(4) NOT NULL DEFAULT 0 C.
+2014-06-10T23:28:47+00:00	INFO	update	Ran query from file 3.3.0-2014-04-02. Query text: INSERT INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder`.
+2014-06-10T23:28:47+00:00	INFO	update	Deleting removed files and folders.
+2014-06-10T23:28:59+00:00	INFO	update	Cleaning up after installation.
+2014-06-10T23:28:59+00:00	INFO	update	Update to version 3.3.0 is complete.
+2014-06-18T06:00:27+00:00	INFO	update	Update started by user Super User (307). Old version is 3.3.0.
+2014-06-18T06:00:27+00:00	INFO	update	Downloading update file from .
+2014-06-18T06:00:31+00:00	INFO	update	File Joomla_3.3.x_to_3.3.1-Stable-Patch_Package.zip successfully downloaded.
+2014-06-18T06:00:31+00:00	INFO	update	Starting installation of new version.
+2014-06-18T06:00:35+00:00	INFO	update	Finalising installation.
+2014-06-18T06:00:36+00:00	INFO	update	Deleting removed files and folders.
+2014-06-18T06:00:45+00:00	INFO	update	Cleaning up after installation.
+2014-06-18T06:00:45+00:00	INFO	update	Update to version 3.3.1 is complete.
+2014-09-11T20:50:07+00:00	INFO	update	Update started by user Super User (307). Old version is 3.3.1.
+2014-09-11T20:50:07+00:00	INFO	update	Downloading update file from .
+2014-09-11T20:50:16+00:00	INFO	update	File Joomla_3.3.x_to_3.3.3-Stable-Patch_Package.zip successfully downloaded.
+2014-09-11T20:50:16+00:00	INFO	update	Starting installation of new version.
+2014-09-11T20:50:27+00:00	INFO	update	Finalising installation.
+2014-09-11T20:50:27+00:00	INFO	update	Deleting removed files and folders.
+2014-09-11T20:50:38+00:00	INFO	update	Cleaning up after installation.
+2014-09-11T20:50:38+00:00	INFO	update	Update to version 3.3.3 is complete.
+2014-10-07T23:17:33+00:00	INFO	update	Update started by user Super User (307). Old version is 3.3.3.
+2014-10-07T23:17:33+00:00	INFO	update	Downloading update file from .
+2014-10-07T23:17:38+00:00	INFO	update	File Joomla_3.3.x_to_3.3.6-Stable-Patch_Package.zip successfully downloaded.
+2014-10-07T23:17:38+00:00	INFO	update	Starting installation of new version.
+2014-10-07T23:17:47+00:00	INFO ::1	update	Finalising installation.
+2014-10-07T23:17:48+00:00	INFO ::1	update	Ran query from file 3.3.4-2014-08-03. Query text: ALTER TABLE `#__user_profiles` CHANGE `profile_value` `profile_value` TEXT NOT N.
+2014-10-07T23:17:48+00:00	INFO ::1	update	Ran query from file 3.3.6-2014-09-30. Query text: INSERT INTO `#__update_sites` (`name`, `type`, `location`, `enabled`) VALUES ('J.
+2014-10-07T23:17:48+00:00	INFO ::1	update	Ran query from file 3.3.6-2014-09-30. Query text: INSERT INTO `#__update_sites_extensions` (`update_site_id`, `extension_id`) VALU.
+2014-10-07T23:17:48+00:00	INFO ::1	update	Deleting removed files and folders.
+2014-10-07T23:17:57+00:00	INFO ::1	update	Cleaning up after installation.
+2014-10-07T23:17:57+00:00	INFO ::1	update	Update to version 3.3.6 is complete.
